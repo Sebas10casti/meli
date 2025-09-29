0d0e071587e7dc4ce40a2e69663df850fed2e441
Sebastian Castiblanco <sebas07casti@gmail.com>
2025-09-28 23:17:50 -0500

    Update Jest configuration to lower coverage thresholds for branches, functions, lines, and statements from 70% to 25%.

--- jest.config.js
+++ jest.config.js
@@ -18,10 +18,10 @@ const customJestConfig = {
   ],
   coverageThreshold: {
     global: {
-      branches: 70,
-      functions: 70,
-      lines: 70,
-      statements: 70,
+      branches: 25,
+      functions: 25,
+      lines: 25,
+      statements: 25,
     },
   },
 }
